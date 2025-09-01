346435fbcfee44f5878d9f648d34151dd039435a
jayateja0 <jayatejaketharaju@gmail.com>
2025-09-01 15:21:30 +0530

    Dockre- practice

---
+++ app.py
@@ -0,0 +1,8 @@
+import random
+print("Welcome to the Number Guessing Game!")
+secret_number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+if guess == secret_number:
+    print(" Hurrayyyy! You guessed it right!")
+else:
+    print(f" Wrong guess. The number was {secret_number}.")
